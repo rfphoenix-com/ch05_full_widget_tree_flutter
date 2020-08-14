49d9e2310106df9208401729f3446a9216110e82
Johnson Wang <freelance@rfphoenix.com>
2020-08-13 18:40:20 -0700

    flatten the widget tree by refactor the deep widget tree into multiple widget tree class.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,101 +20,134 @@ class _HomeState extends State<Home> {
           padding: EdgeInsets.all(16.0),
           child: Column(
             children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Container(
-                    color: Colors.yellow,
-                    height: 40.0,
-                    width: 40.0,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(16.0),
-                  ),
-                  Expanded(
-                    child: Container(
-                      color: Colors.amber,
-                      height: 40.0,
-                      width: 40.0,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(16.0),
-                  ),
-                  Container(
-                    color: Colors.brown,
-                    height: 40.0,
-                    width: 40,
-                  ),
-                ],
-              ),
+              const RowWidget(),
               Padding(
                 padding: EdgeInsets.all(16.0),
               ),
-              Row(
-                children: <Widget>[
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      Container(
-                        color: Colors.yellow,
-                        height: 60.0,
-                        width: 60.0,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(16.0),
-                      ),
-                      Container(
-                        color: Colors.amber,
-                        height: 40.0,
-                        width: 40.0,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(16.0),
-                      ),
-                      Container(
-                        color: Colors.brown,
-                        height: 20.0,
-                        width: 20.0,
-                      ),
-                      Divider(),
-                      Row(
-                        children: <Widget>[
-                          CircleAvatar(
-                            backgroundColor: Colors.lightGreen,
-                            radius: 100.0,
-                            child: Stack(
-                              children: <Widget>[
-                                Container(
-                                  height: 100.0,
-                                  width: 100.0,
-                                  color: Colors.yellow,
-                                ),
-                                Container(
-                                  height: 60.0,
-                                  width: 60.0,
-                                  color: Colors.amber,
-                                ),
-                                Container(
-                                  height: 40.0,
-                                  width: 40.0,
-                                  color: Colors.brown,
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                      Divider(),
-                      Text('End of the Line'),
-                    ],
-                  )
-                ],
-              )
+              const RowAndColumnWidget(),
             ],
           ),
         ),
       )),
+    );
+  }
+}
+
+class RowAndColumnWidget extends StatelessWidget {
+  const RowAndColumnWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              color: Colors.yellow,
+              height: 60.0,
+              width: 60.0,
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+            ),
+            Container(
+              color: Colors.amber,
+              height: 40.0,
+              width: 40.0,
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+            ),
+            Container(
+              color: Colors.brown,
+              height: 20.0,
+              width: 20.0,
+            ),
+            Divider(),
+            const RowAndStackWidget(),
+            Divider(),
+            Text('End of the Line. Date: ${DateTime.now()}'),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class RowAndStackWidget extends StatelessWidget {
+  const RowAndStackWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        CircleAvatar(
+          backgroundColor: Colors.lightGreen,
+          radius: 100.0,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                height: 60.0,
+                width: 60.0,
+                color: Colors.amber,
+              ),
+              Container(
+                height: 40.0,
+                width: 40.0,
+                color: Colors.brown,
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class RowWidget extends StatelessWidget {
+  const RowWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          color: Colors.yellow,
+          height: 40.0,
+          width: 40.0,
+        ),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.amber,
+            height: 40.0,
+            width: 40.0,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+        ),
+        Container(
+          color: Colors.brown,
+          height: 40.0,
+          width: 40,
+        ),
+      ],
     );
   }
 }
